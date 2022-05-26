39d6f776f8b6a03b10f9eb889837dca011f6c27a
jeonghoonji <wlwjdgns0920@naver.com>
2022-05-26 23:27:37 +0900

    xcode13버전으로 오류가 있을수있음

---
+++ MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by 지정훈 on 2022/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
