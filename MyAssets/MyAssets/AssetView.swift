7e5db5c5bd9cecbcdaceb660787baf5df126b41d
jeonghoonji <wlwjdgns0920@naver.com>
2022-05-27 17:38:24 +0900

    그리드뷰 만들기

---
+++ MyAssets/MyAssets/AssetView.swift
@@ -0,0 +1,30 @@
+//
+//  AssetView.swift
+//  MyAssets
+//
+//  Created by 지정훈 on 2022/05/27.
+//
+
+import SwiftUI
+
+struct AssetView: View {
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                VStack(spacing:20){
+                    // 빈공간 만듬
+                    Spacer()
+                    AssetMenuGridView()
+                }
+            }
+            .background(Color.gray.opacity(0.2))
+            .navigationBarWithButtonStyle("내자산")
+        }
+    }
+}
+
+struct AssetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetView()
+    }
+}
